6b47ba56b664fa6b188f1cd27eb816c2b6490421
FrancescoVenanti <francesco.venanti@gmail.com>
2024-09-13 12:25:03 +0200

    marcomerda

--- index.ts
+++ index.ts
@@ -1,7 +1,21 @@
+import { Canvas } from "./src/canvas";
+
 (() => {
   const container = document.getElementById("container");
   const canvas = document.createElement("canvas");
   container?.appendChild(canvas);
   Canvas.init(canvas);
-  Canvas.line(100, 100);
+  Canvas.line(
+    {
+      point1: [0, 0],
+      point2: [100, 100],
+      color: "red",
+      width: 5,
+    },
+    {
+      point1: [120, 60],
+      point2: [200, 200],
+      color: "blue",
+    }
+  );
 })();
--- src/canvas.ts
+++ src/canvas.ts
@@ -1,4 +1,15 @@
-class Canvas {
+import { TupleType } from "typescript";
+
+/* || */
+
+interface CanvasLinesProps {
+  point1?: [number, number];
+  point2: [number, number];
+  color?: string;
+  width?: number;
+}
+
+export class Canvas {
   private static canvas: HTMLCanvasElement;
   private static ctx: CanvasRenderingContext2D;
 
@@ -7,11 +18,20 @@ class Canvas {
     Canvas.ctx = canvas.getContext("2d")!;
   }
 
-  static line(n1: number, n2: number): void {
-    Canvas.ctx.beginPath();
-    Canvas.ctx.moveTo(0, 0);
-    Canvas.ctx.lineTo(n1, n2);
-    Canvas.ctx.stroke();
+  static line(...properties: CanvasLinesProps[]) {
+    let nextStart: [number, number] = [0, 0];
+    properties.forEach(({ point1, point2, color, width }) => {
+      Canvas.ctx.beginPath();
+      Canvas.ctx.moveTo(
+        point1 ? point1[0] : nextStart[0],
+        point1 ? point1[1] : nextStart[1]
+      );
+      Canvas.ctx.lineTo(point2[0], point2[1]);
+      nextStart = point2;
+      Canvas.ctx.strokeStyle = color ?? "black";
+      Canvas.ctx.lineWidth = width ?? 1;
+      Canvas.ctx.stroke();
+    });
   }
   static circle(): void {}
   static rect(): void {}
