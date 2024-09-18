b6df67ed3e8928343c564ff88d5bccce8352c606
marcomit <marcomenegazzi.05@gmail.com>
2024-09-18 08:36:23 +0200

    delta

--- src/index.ts
+++ src/index.ts
@@ -24,11 +24,16 @@ const player = new Sprite("placeholder", "idle", 3, new Vector(0, 0));
     )
   ).then(() => loop());
 })();
+let delay1 = 0;
+let delay2 = 0;
+
 function loop(delay?: number) {
+  delay2 = delay1;
+  delay1 = delay;
   Canvas.ctx.clearRect(0, 0, Canvas.canvas.width, Canvas.canvas.height);
   let dx = 0;
   let dy = 0;
-
+  console.log(delay1 - delay2);
   if (keyPressed.has("w")) dy -= 1;
   if (keyPressed.has("s")) dy += 1;
   if (keyPressed.has("a")) dx -= 1;
