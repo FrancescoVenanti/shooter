61309a51b9e1569b1a63c0367785b9c49e25cae3
FrancescoVenanti <francesco.venant@gmail.com>
2024-09-25 17:28:40 +0200

    aaa

--- src/entities/enemy.ts
+++ src/entities/enemy.ts
@@ -3,16 +3,16 @@ import { Asset } from "../lib/global";
 import Character from "./character";
 
 class Enemy extends Character {
-
-  constructor(sprite: keyof Asset["character"], action: keyof Asset["character"][keyof Asset["character"]], position: Vector = new Vector(0, 0), angle: number = 0,  life: number = 100) {
+  constructor(
+    sprite: keyof Asset["character"],
+    action: keyof Asset["character"][keyof Asset["character"]],
+    position: Vector = new Vector(0, 0),
+    angle: number = 0,
+    life: number = 100
+  ) {
     super(sprite, action, position, life);
     this.angle = angle;
   }
-
-  public draw() {
-    super.draw();
-    this.darkenPlayer();
-  }
 }
 
-export default Enemy
+export default Enemy;
