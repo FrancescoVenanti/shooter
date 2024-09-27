4b1d5f9b8d63adeca0b4b070ae19010c79b72320
FrancescoVenanti <francesco.venant@gmail.com>
2024-09-27 15:45:54 +0200

    dioca NOPSFDGBIOUVGWEO G

--- src/entities/enemy.ts
+++ src/entities/enemy.ts
@@ -1,8 +1,12 @@
+import { Environment } from "../core/environment";
 import Vector from "../core/vector";
 import { Asset, GLOBAL } from "../lib/global";
 import Character from "./character";
+import Ranged from "./weapons/ranged";
+import Weapon from "./weapons/weapon";
 
 class Enemy extends Character {
+  public primaryWeapon: Weapon;
   constructor(
     sprite: keyof Asset["character"],
     action: keyof Asset["character"][keyof Asset["character"]],
@@ -12,10 +16,24 @@ class Enemy extends Character {
   ) {
     super(sprite, action, position, life);
     this.angle = angle;
+    const primaryWeapon: Weapon = new Ranged({
+      projectileImage: "book",
+      rect: this.rect,
+      damage: 100,
+      range: 100,
+      speed: 1,
+      rate: 1,
+      reloadTime: 1,
+      magazineSize: 1,
+    });
+    this.primaryWeapon = primaryWeapon;
   }
 
   public draw() {
     super.draw(GLOBAL("POSITION"));
+    for (const projectile of this.primaryWeapon.bullets) {
+      projectile.draw();
+    }
   }
 }
 
